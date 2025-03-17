05d4671b31d550dae847bd430fa8d698f5fc6f4f
Jsd2024 <anindita5151@gmail.com>
2025-03-17 21:54:57 +0530

    consumerB added

--- src/main/java/com/consumer/service/ConsumerService.java
+++ src/main/java/com/consumer/service/ConsumerService.java
@@ -7,21 +7,21 @@ import org.springframework.stereotype.Service;
 public class ConsumerService {
 
     // Listener for the common-topic
-    @KafkaListener(topics = "common-topic", groupId = "my-consumer-group")
+    @KafkaListener(topics = "common-topic", groupId = "my-consumer-group-2")
     public void consumeCommonTopicMessage(String message) {
         System.out.println("Received message from common-topic: " + message);
     }
 
-    // Listener for the even-topic used in ConsumerA
-    @KafkaListener(topics = "even-topic", groupId = "my-consumer-group")
-    public void consumeEvenTopicMessage(String message) {
-        System.out.println("Received message from even-topic: " + message);
-    }
-
-//    // Listener for the odd-topic used in ConsumerB
-//    @KafkaListener(topics = "odd-topic", groupId = "my-consumer-group")
-//    public void consumeOddTopicMessage(String message) {
-//        System.out.println("Received message from odd-topic: " + message);
+//    // Listener for the even-topic used in ConsumerA
+//    @KafkaListener(topics = "even-topic", groupId = "my-consumer-group")
+//    public void consumeEvenTopicMessage(String message) {
+//        System.out.println("Received message from even-topic: " + message);
 //    }
+
+    // Listener for the odd-topic used in ConsumerB
+    @KafkaListener(topics = "odd-topic", groupId = "my-consumer-group")
+    public void consumeOddTopicMessage(String message) {
+        System.out.println("Received message from odd-topic: " + message);
+    }
 
 }
